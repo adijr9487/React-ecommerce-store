7b6c35cbeadbb0daed40d09eb999b54d0d33972d
Thrillophilia <adijr9487@gmail.com>
2023-02-23 16:30:41 +0530

    :sparkles: added sidebar

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,63 +1,17 @@
 import * as React from "react";
 import PropTypes from "prop-types";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import CssBaseline from "@mui/material/CssBaseline";
-import Divider from "@mui/material/Divider";
-import Drawer from "@mui/material/Drawer";
-import IconButton from "@mui/material/IconButton";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
-import MailIcon from "@mui/icons-material/Mail";
-import MenuIcon from "@mui/icons-material/Menu";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import Navbar from "../Navbar/Navbar";
+import { Box, CssBaseline, Drawer } from "@mui/material";
 
-function ResponsiveDrawer(props) {
+import Filter from "../Filter/Filter";
+
+const Sidebar = (props) => {
   const { window } = props;
-
-  const drawer = (
-    <div>
-      <Toolbar />
-      <Divider />
-      <List>
-        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-      <Divider />
-      <List>
-        {["All mail", "Trash", "Spam"].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-    </div>
-  );
 
   const container =
     window !== undefined ? () => window().document.body : undefined;
 
   return (
-    <Box sx={{ display: "flex" }}>
+    <>
       <CssBaseline />
       <Box
         component="nav"
@@ -81,7 +35,7 @@ function ResponsiveDrawer(props) {
             },
           }}
         >
-          {drawer}
+          <Filter filter={props.filter} setFilter={props.setFilter}/>
         </Drawer>
         <Drawer
           variant="permanent"
@@ -94,25 +48,14 @@ function ResponsiveDrawer(props) {
           }}
           open
         >
-          {drawer}
+          <Filter filter={props.filter} setFilter={props.setFilter}/>
         </Drawer>
       </Box>
-      <Box
-        component="main"
-        sx={{
-          flexGrow: 1,
-          p: 3,
-          width: { sm: `calc(100% - ${props.drawerWidth}px)` },
-        }}
-      >
-        <Toolbar />
-        {/* Main product section */}
-      </Box>
-    </Box>
+    </>
   );
-}
+};
 
-ResponsiveDrawer.propTypes = {
+Sidebar.propTypes = {
   /**
    * Injected by the documentation to work in an iframe.
    * You won't need it on your project.
@@ -120,4 +63,4 @@ ResponsiveDrawer.propTypes = {
   window: PropTypes.func,
 };
 
-export default ResponsiveDrawer;
+export default Sidebar;
